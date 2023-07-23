30286a37ef804e9e9fc0faf7ddcaaa3e103ee80a
sewonjun <93499071+sewonjun@users.noreply.github.com>
2023-07-23 17:59:05 +0900

    feat: 로그인 기능 구현 커밋

--- app.js
+++ app.js
@@ -3,34 +3,37 @@ require("dotenv").config();
 const createError = require("http-errors");
 const express = require("express");
 const mongoose = require("mongoose");
-// const __dirname = path.resolve();
+const cors = require("cors");
 // eslint-disable-next-line no-undef
 const url = process.env.DB_URL;
 
 const login = require("./routes/login");
+const index = require("./routes/index");
 
 const app = express();
 
-// view engine setup
+let corsOptions = {
+  origin: ["http://localhost:5173"],
+  credentials: true,
+};
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors(corsOptions));
 
 mongoose.connect(url);
 
-app.use("/", login);
+app.use("/", index);
+app.use("/login", login);
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function (err, req, res) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
+  console.log("err", err);
   res.status(err.status || 500);
   res.json();
 });
