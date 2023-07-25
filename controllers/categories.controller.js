ee1934e02772a896acb1b95e0da9a414912e6aa6
sewonjun <93499071+sewonjun@users.noreply.github.com>
2023-07-25 13:00:33 +0900

    feat: feature/NewResourceForm s3 저장 로직 구현 및 svg png 변환 로직 구현

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -1,51 +1,93 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
-const { uploadObject } = require("../utils/s3");
+const { uploadObject, convertImage } = require("../utils/s3");
 const ResourceVersion = require("../models/ResourceVersion");
-// const Resource = require("../models/Resource");
+const Resource = require("../models/Resource");
 const User = require("../models/User");
+const Category = require("../models/Category");
 
 const resource = async function (req, res, next) {
   const data = req.body;
-  const user = await User.findOne({ email: data.detail.email });
-  console.log(user);
-  // uploadObject(req.body.name, req.body.files[0].file.svgFile, "image/svg+xml");
-  const resourceVersion = await ResourceVersion.findOne({
-    name: data.name,
-    detail: { version: "1.0.0" },
-  });
-  console.log(resourceVersion);
-  const newResourceVersion = await new ResourceVersion({
-    name: data.name,
-    categoryId: data.categoryId,
-    detail: {
-      version: "1.0.0",
-      uploadDate: data.detail.uploadDate,
-      author: user._id,
-    },
-    files: data.files.map(el => {
-      return {
-        file: {
-          fileName: el.file.fileName,
-        },
-      };
-    }),
-  }).save();
 
-  const newResourceVersionObjectId = JSON.stringify(newResourceVersion._id);
+  try {
+    const user = await User.findOne({ email: data.detail.email });
+    const resourceVersion = await ResourceVersion.findOne({
+      name: data.name,
+    });
 
-  data.files.forEach((el, index) => {
-    const uploadName =
-      newResourceVersionObjectId + "/" + el.file.fileName + ".svg";
-    console.log("uploadName", uploadName);
-    uploadObject(uploadName, el.file.svgFile, "image/svg+xml");
-    newResourceVersion.files[index].file.svgUrl =
-      "https://team-dbx.s3.ap-northeast-2.amazonaws.com/%" + uploadName;
-  });
+    if (resourceVersion) {
+      throw new Error("Resource Name Exist");
+    }
 
-  await newResourceVersion.save();
-  // try {
-  // } catch (error) {}
+    const newResourceVersion = await new ResourceVersion({
+      name: data.name,
+      categoryId: data.categoryId,
+      detail: {
+        version: "1.0.0",
+        uploadDate: data.detail.uploadDate,
+        author: user._id,
+      },
+      files: data.files.map(el => {
+        return {
+          file: {
+            fileName: el.file.fileName,
+          },
+        };
+      }),
+    }).save();
+
+    const newResourceVersionObjectId = JSON.stringify(newResourceVersion._id);
+
+    const uploadPromises = data.files.map(async (el, index) => {
+      const uploadNameSVG =
+        newResourceVersionObjectId + "/" + el.file.fileName + ".svg";
+      const uploadNamePNG =
+        newResourceVersionObjectId + "/" + el.file.fileName + ".png";
+      newResourceVersion.files[index].file.svgUrl =
+        "https://team-dbx.s3.ap-northeast-2.amazonaws.com/" + uploadNameSVG;
+      newResourceVersion.files[index].file.pngUrl =
+        "https://team-dbx.s3.ap-northeast-2.amazonaws.com/" + uploadNamePNG;
+
+      const convertedPNG = await convertImage(el.file.svgFile);
+
+      return (
+        uploadObject(uploadNameSVG, el.file.svgFile, "image/svg+xml"),
+        uploadObject(uploadNamePNG, convertedPNG, "image/png")
+      );
+    });
+
+    newResourceVersion.save();
+
+    await Promise.all(uploadPromises);
+
+    const newResource = await new Resource({
+      name: data.name,
+      categoryId: data.categoryId,
+      currentVersion: newResourceVersion._id,
+    }).save();
+
+    newResource.versions.push(newResourceVersion._id);
+    newResource.save();
+
+    const category = await Category.findOne({ _id: data.categoryId });
+    category.resources.push(newResource._id);
+    category.save();
+
+    res.statusCode = 201;
+    res.json({ result: "ok" });
+  } catch (error) {
+    if (error.message === "Resource Name Exist") {
+      res.statusCode = 409;
+      res.json({
+        result: "error",
+        message: "409 Resource Name Exist",
+      });
+
+      return;
+    }
+
+    next(error);
+  }
 };
 
 module.exports = {
